f4ba4f1a82b99cca75d3326c52f2c7c1ca0c3f5e
Nitin Chaurasia <gs.nitin@gmail.com>
2018-08-14 22:01:49 -0700

    Python Learning resumes

---
+++ TakeABreak/sms.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 14 21:53:26 2018
+
+@author: nitin
+"""
+
+import twilio
+
+print (twilio.__version__)
--- TakeABreak/takeABreak.py
+++ TakeABreak/takeABreak.py
@@ -6,7 +6,10 @@ duration = 2*60*60
 #duration = 2
 
 n_times = 6
-while(n_times > 1 ):        
+
+print ("The Timer Started at " +time.ctime())
+
+while(n_times > 0 ):        
     time.sleep(duration)
     webbrowser.open("https://www.youtube.com/watch?v=-qlJiGGvPUI")
     n_times = n_times-1
