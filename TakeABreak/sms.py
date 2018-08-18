d6b16a8d038b521be6d246ef8bbd2bb385cb18a3
Nitin Chaurasia <gs.nitin@gmail.com>
2018-08-17 23:18:12 -0700

    take a break

--- TakeABreak/sms.py
+++ TakeABreak/sms.py
@@ -9,3 +9,20 @@ Created on Tue Aug 14 21:53:26 2018
 import twilio
 
 print (twilio.__version__)
+
+# Download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+account_sid = 'AC56e0753aee90cf5e8f5391576cb6d25f'
+auth_token = '0d26fc6ec3e623ac96e0b691b65e3f53'
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+                              from_='+1(650) 480-1846',
+                              body='body',
+                              to='+14083731743'
+                          )
+
+print(message.sid)
